a37a68555173dbeaf3e7c35cf9c8087895bca16a
Joel Chen <joel123@gmail.com>
2026-07-10 19:05:16 -0700

    CFA-16: exclude development examples from package

---
+++ dev-tools/create-fynapp/test/spec/package-artifact.test.ts
@@ -0,0 +1,12 @@
+import fs from "node:fs";
+import path from "node:path";
+
+const packageDir = path.resolve(__dirname, "../..");
+
+describe("published package artifact", () => {
+  it("does not include development examples", () => {
+    const pkg = JSON.parse(fs.readFileSync(path.join(packageDir, "package.json"), "utf-8"));
+
+    expect(pkg.files).not.toContain("examples");
+  });
+});
